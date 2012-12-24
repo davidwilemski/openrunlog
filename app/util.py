184b3439602eb1e2fe1ea069c4a58b200b9b1b6d
David Wilemski <david@davidwilemski.com>
2012-12-23 23:22:12 -0500

    use hashlib

--- app/util.py
+++ app/util.py
@@ -1,6 +1,6 @@
 
 import bcrypt
-import md5
+import hashlib
 
 def hash_pwd(password):
     return bcrypt.hashpw(password, bcrypt.gensalt())
@@ -11,8 +11,7 @@ def check_pwd(password, hashed):
 def validate_time(time):
     return True 
 
-# XXX md5 module deprecated, use hashlib
 def gravatar_html(email):
-    h = md5.md5(email.lower()).hexdigest()
+    h = hashlib.md5(email.lower()).hexdigest()
     html = '<img src="http://www.gravatar.com/avatar/%s.jpg?s=15" />' % h
     return html
