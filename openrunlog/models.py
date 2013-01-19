3ad5947faf3eca094d3914a9d3fb03c8ae8cf506
David Wilemski <david@davidwilemski.com>
2013-01-18 22:08:22 -0500

    show pace on dashboard rather than time

--- openrunlog/models.py
+++ openrunlog/models.py
@@ -45,7 +45,7 @@ def time_to_seconds(time):
         raise ValueError('Time not in correct format')
     return seconds
 
-def seconds_to_time(seconds):
+def seconds_to_time(seconds, pace=False):
     hours = 0
     minutes = 0
     while seconds > 59:
@@ -56,6 +56,8 @@ def seconds_to_time(seconds):
         hours += 1
     
     if hours == 0:
+        if pace:
+            return '{:01}:{:02}'.format(minutes, seconds)
         return '00:{:02}:{:02}'.format(minutes, seconds)
     else:
         return '{}:{:02}:{:02}'.format(hours, minutes, seconds)
@@ -85,6 +87,12 @@ class Run(mongoengine.Document):
     distance = mongoengine.FloatField()
     time = mongoengine.IntField() # store time in seconds for easy manipulation
     notes = mongoengine.StringField()
+
+    @property
+    def pace(self):
+        if not self.time:
+            return 'N/A'
+        return seconds_to_time(int(self.time/self.distance), pace=True) + ' pace'
 
     @classmethod
     def this_week_runs(cls, user):
