cd6b6a5a6e733ee0de47ec775f235ec9f9a33968
David Wilemski <david@davidwilemski.com>
2013-04-28 15:01:13 -0400

    add ability to enter distance + pace instead of distance + time for adding a run

--- openrunlog/runs.py
+++ openrunlog/runs.py
@@ -2,6 +2,7 @@
 import base
 import crosspost
 import constants
+import logging
 import models
 import util
 import datetime
@@ -17,9 +18,14 @@ class AddRunHandler(base.BaseHandler):
         date = self.get_argument('date', '')
         date = dateutil.parser.parse(date, fuzzy=True)
         distance = self.get_argument('distance', '')
+        distance = float(distance)
         time = self.get_argument('time', '0')
+        pacetime = self.get_argument('pacetime', 'time')
+
         try:
             time = models.time_to_seconds(time)
+            if pacetime == 'pace':
+                time = int(time*distance)
         except ValueError, e:
             msg = "The value you entered for time was not valid. Please enter your time in format HH:MM:SS or MM:SS or MM."
             self.redirect_msg('/u/%s' % user.url, {'error': msg})
@@ -27,7 +33,6 @@ class AddRunHandler(base.BaseHandler):
         notes = self.get_argument('notes', '')
         user = self.get_current_user()
 
-        distance = float(distance)
         if not util.validate_time(time):
             self.finish('error, go back and try again')
 
