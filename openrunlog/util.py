05747ace6776d4320f9780f7e1fd00655de2c17e
David Wilemski <david@davidwilemski.com>
2013-04-28 15:42:50 -0400

    removing the tooltip for notes because it's unreadable

--- openrunlog/util.py
+++ openrunlog/util.py
@@ -8,9 +8,6 @@ def hash_pwd(password):
 def check_pwd(password, hashed):
     return bcrypt.hashpw(password, hashed) == hashed
 
-def validate_time(time):
-    return True 
-
 def gravatar_html(email, size=15):
     h = hashlib.md5(email.lower()).hexdigest()
     html = '<img src="http://www.gravatar.com/avatar/{}.jpg?s={}" />'.format(h, size)
