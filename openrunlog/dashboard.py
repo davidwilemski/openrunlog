90c8b19485bc34868eb30fa34a9a99e6fc121cd0
David Wilemski <david@davidwilemski.com>
2013-07-28 16:40:41 -0700

    remove unused query

--- openrunlog/dashboard.py
+++ openrunlog/dashboard.py
@@ -25,8 +25,6 @@ class ProfileHandler(base.BaseHandler):
             models.Run.get_recent_runs, profile, 10)
         week = yield self.execute_thread(
             models.Week.this_week, profile)
-        miles_this_week = yield self.execute_thread(
-            models.Run.this_week_mileage, profile)
         year = datetime.date.today().year
 
-        self.render('dashboard.html', page_title='Dashboard', user=user, recent_runs=recent_runs, today=datetime.date.today().strftime("%x"), error=error, miles_this_week=miles_this_week, week=week, this_year=year, profile=profile)
+        self.render('dashboard.html', page_title='Dashboard', user=user, recent_runs=recent_runs, today=datetime.date.today().strftime("%x"), error=error, week=week, this_year=year, profile=profile)
