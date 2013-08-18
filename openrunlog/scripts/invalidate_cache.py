a396f43fac4f1c1fa72014a1d579569ec05def0b
David Wilemski <david@davidwilemski.com>
2013-08-18 12:57:08 -0700

    setup mongo connection on invalidate_cache script

--- openrunlog/scripts/invalidate_cache.py
+++ openrunlog/scripts/invalidate_cache.py
@@ -1,8 +1,19 @@
 
-import tornadoredis
+import env
+import mongoengine
 from openrunlog import models, cache
+import tornadoredis
 
 if __name__ == '__main__':
+    config = env.prefix('ORL_')
+    if config['debug'] == 'True':
+        config['debug'] = True
+    else:
+        config['debug'] = False
+    mongoengine.connect(
+        config['db_name'],
+        host=config['db_uri'])
+
     r = tornadoredis.Client()
     r.connect()
 
