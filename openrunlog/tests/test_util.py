8d04d7d88722e92c63310141365eecb934c387d5
David Wilemski <david@davidwilemski.com>
2013-12-27 01:57:06 -0500

    finish test coverage of util - test big robo img

--- openrunlog/tests/test_util.py
+++ openrunlog/tests/test_util.py
@@ -16,6 +16,7 @@ class BCryptTests(unittest.TestCase):
 class ImageHTMLTests(unittest.TestCase):
     expected_fb_url = 'https://graph.facebook.com/1264253926/picture?type=small'
     expected_robo_url = 'https://robohash.org/dtwwtd@gmail.com.jpg?gravatar=yes&size=50x50'
+    expected_bigrobo_url = 'https://robohash.org/dtwwtd@gmail.com.jpg?gravatar=yes&size=180x180'
 
     def test_fb_image_url(self):
         url = util.fb_image_url(fbid)
@@ -29,6 +30,7 @@ class ImageHTMLTests(unittest.TestCase):
         expected_base_html = '<img src="{}" />'
         expected_fb_html = expected_base_html.format(self.expected_fb_url)
         expected_robo_html = expected_base_html.format(self.expected_robo_url)
+        expected_bigrobo_html = expected_base_html.format(self.expected_bigrobo_url)
 
         fbuser = models.User(display_name='david', email=email)
         fbuser.facebook['id'] = fbid
@@ -36,6 +38,8 @@ class ImageHTMLTests(unittest.TestCase):
 
         fb_html = util.image_html(fbuser, 'small')
         robo_html = util.image_html(robouser, 'small')
+        bigrobo_html = util.image_html(robouser, 'big')
 
         self.assertEqual(expected_fb_html, fb_html)
         self.assertEqual(expected_robo_html, robo_html)
+        self.assertEqual(expected_bigrobo_html, bigrobo_html)
