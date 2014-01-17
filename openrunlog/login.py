2fb358f4fa9130154459b43f5ac9a8f748c6ecaf
David Wilemski <david@davidwilemski.com>
2014-01-17 12:45:21 -0500

    remove URL changing in settings, style settings.py

--- openrunlog/login.py
+++ openrunlog/login.py
@@ -25,7 +25,7 @@ class LoginHandler(base.BaseHandler):
     @web.asynchronous
     @gen.engine
     def post(self):
-        email = self.get_argument('email', '')
+        email = self.get_argument('email', '').lower()
         password = self.get_argument('password', '')
         error = False
 
@@ -45,10 +45,11 @@ class LoginHandler(base.BaseHandler):
 
         if error:
             error_text = "Yo! You gave an invalid username or incorrect password!"
-            self.render('login.html', page_title='Log In', user=None, error=error_text)
+            self.render('login.html', page_title='Log In',
+                        user=None, error=error_text)
             self.tf.send({'users.logins.failure': 1}, lambda x: x)
             return
-        
+
         cookie_args = {'httponly': True}
 
         logging.debug('request.protocol is {}'.format(self.request.protocol))
@@ -67,16 +68,17 @@ class RegisterHandler(base.BaseHandler):
         user = yield self.get_current_user_async()
         if user is not None:
             self.redirect('/')
-        self.render('register.html', page_title='Register', user=user, error='')
+        self.render('register.html', page_title='Register',
+                    user=user, error='')
 
     @web.asynchronous
     @gen.coroutine
     def post(self):
-        email = self.get_argument('email', '')
+        email = self.get_argument('email', '').lower()
         display_name = self.get_argument('display_name', '')
         password = self.get_argument('password', '')
         password_confirm = self.get_argument('password_confirm', '')
-        url = self.get_argument('url', '')
+        url = self.get_argument('url', '').lower()
         public = True if self.get_argument('public', '') == 'yes' else False
 
         # check for general errors
@@ -87,20 +89,23 @@ class RegisterHandler(base.BaseHandler):
             error = True
         if error:
             error_text = 'Missing a field or passwords do not match, all fields are required.'
-            self.render('register.html', page_title='Register', user=None, error=error_text)
+            self.render('register.html', page_title='Register',
+                        user=None, error=error_text)
             return
 
         # check that email hasn't already been used
         check_user = yield models.get_user_by_email(self.redis, email)
         if check_user:
             error_text = 'The email you tried already has an account. Please log in or register with a different email address.'
-            self.render('register.html', page_title='Register', user=None, error=error_text)
+            self.render('register.html', page_title='Register',
+                        user=None, error=error_text)
             return
- 
+
         # make sure url is unique
         if url and not models.url_unique(url):
             error_text = 'A unique profile URL is required for public accounts!'
-            self.render("register.html", page_title="Register", user=None, error=error_text)
+            self.render("register.html", page_title="Register",
+                        user=None, error=error_text)
 
         new_user = models.User(email=email)
         new_user.password = util.hash_pwd(password)
@@ -122,50 +127,50 @@ class LogoutHandler(base.BaseHandler):
         yield gen.Task(self.tf.send, {'users.logouts': 1})
         self.redirect('/')
 
+
 class SettingsHandler(base.BaseHandler):
     @base.authenticated_async
     @web.asynchronous
     @gen.coroutine
     def get(self):
         user = yield self.get_current_user_async()
-        self.render("settings.html", page_title="Settings for {}".format(user.display_name), user=user, error=None)
+        self.render("settings.html",
+                    page_title="Settings for {}".format(user.display_name),
+                    user=user, error=None)
 
     @base.authenticated_async
     @web.asynchronous
     @gen.coroutine
     def post(self):
         user = yield self.get_current_user_async()
-        url = self.get_argument('url', '')
         display_name = self.get_argument('displayname', '')
-        url = self.get_argument('url', '')
         public = self.get_argument('public', '')
         hashtags = self.get_argument('hashtags', '')
 
         if not display_name:
             error = 'A display name is required!'
-            self.render("settings.html", page_title="Settings for {}".format(user.display_name), user=user, error=error)
+            self.render("settings.html",
+                        page_title="Settings for {}".format(user.display_name),
+                        user=user, error=error)
             return
 
         public = True if public == 'yes' else False
 
         error = False
-        if url == '':
-            error = 'A profile URL is required for public accounts!'
-            # make sure url is unique
-            if not models.url_unique(url, user):
-                error = 'A profile URL is required for public accounts!'
         if error:
-            self.render("settings.html", page_title="Settings for {}".format(user.display_name), user=user, error=error)
+            self.render("settings.html",
+                        page_title="Settings for {}".format(user.display_name),
+                        user=user, error=error)
             return
 
         user.public = public
-        user.url = url
         user.display_name = display_name
         user.hashtags = hashtags
         user.save(self.redis)
 
         yield gen.Task(self.tf.send, {'users.settings.changed': 1})
         self.redirect('/settings')
+
 
 class DailyMileHandler(base.BaseHandler, auth.OAuth2Mixin):
     _OAUTH_AUTHORIZE_URL = 'https://api.dailymile.com/oauth/authorize'
@@ -174,51 +179,47 @@ class DailyMileHandler(base.BaseHandler, auth.OAuth2Mixin):
     @web.authenticated
     @web.asynchronous
     def get(self):
-        redirect_uri=self.application.config['dailymile_redirect']
-        client_id=self.application.config['dailymile_client_id']
-        client_secret=self.application.config['dailymile_client_secret']
+        redirect_uri = self.application.config['dailymile_redirect']
+        client_id = self.application.config['dailymile_client_id']
+        client_secret = self.application.config['dailymile_client_secret']
 
-
-        logging.debug('auth handler') 
         if self.get_argument("code", None):
-            logging.debug('after redirect') 
+            logging.debug('after redirect')
             self.get_authenticated_user(self._on_auth)
             return
 
-        logging.debug('before redirect') 
         extra = {'response_type': 'code'}
         self.authorize_redirect(
-                redirect_uri=redirect_uri,
-                client_id=client_id,
-                client_secret=client_secret,
-                extra_params=extra)
+            redirect_uri=redirect_uri,
+            client_id=client_id,
+            client_secret=client_secret,
+            extra_params=extra)
 
     @gen.engine
     def get_authenticated_user(self, callback):
         """Fetches the authenticated dailymile user.
         """
-        redirect_uri=self.application.config['dailymile_redirect']
-        client_id=self.application.config['dailymile_client_id']
-        client_secret=self.application.config['dailymile_client_secret']
+        redirect_uri = self.application.config['dailymile_redirect']
+        client_id = self.application.config['dailymile_client_id']
+        client_secret = self.application.config['dailymile_client_secret']
 
         code = self.get_argument("code")
         extra = {
-                'grant_type': 'authorization_code',
+            'grant_type': 'authorization_code',
         }
         url = self._OAUTH_ACCESS_TOKEN_URL
         params = {
-                'code': code,
-                'redirect_uri': redirect_uri,
-                'client_id': client_id,
-                'client_secret': client_secret,
-                'grant_type': 'authorization_code'
+            'code': code,
+            'redirect_uri': redirect_uri,
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'authorization_code'
         }
         client = httpclient.AsyncHTTPClient()
         response = yield gen.Task(client.fetch, url, method='POST',
-                body=urllib.urlencode(params))
+                                  body=urllib.urlencode(params))
         callback(escape.json_decode(response.body))
-    
-    
+
     def _on_auth(self, data):
         user = self.get_current_user()
         user.dailymile_token = data['access_token']
@@ -230,6 +231,7 @@ class DailyMileHandler(base.BaseHandler, auth.OAuth2Mixin):
         # queue past runs for the worker process to cross post
         self.tf.send({'users.dailymile.login': 1}, lambda x: x)
         self.redirect('/settings')
+
 
 class DailyMileLogoutHandler(base.BaseHandler):
     @base.authenticated_async
@@ -243,6 +245,7 @@ class DailyMileLogoutHandler(base.BaseHandler):
 
         yield gen.Task(self.tf.send, {'users.dailymile.logout': 1})
         self.redirect('/settings')
+
 
 class FacebookHandler(base.BaseHandler, auth.FacebookGraphMixin):
     @web.authenticated
