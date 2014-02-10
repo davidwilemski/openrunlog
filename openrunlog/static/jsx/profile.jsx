e219893f9a960774a9cf5538ca494984bb2b65a7
David Wilemski <david@davidwilemski.com>
2014-02-10 13:04:28 -0500

    profile_starter is now a react component

---
+++ openrunlog/static/jsx/profile.jsx
@@ -0,0 +1,42 @@
+/** @jsx React.DOM */
+
+var ProfileSidebar = React.createClass({
+    render: function() {
+        var profile = this.props.profile;
+        var streaks = null;
+        
+        if (profile.streaks) {
+            var mystreaks = profile.streaks;
+            streaks = (<span>
+                <div>Longest Streak: {mystreaks.longest.length} days</div>
+                <div><small>{mystreaks.longest.start} - {mystreaks.longest.end}</small></div>
+                <div>Current Streak: {mystreaks.current.length} days</div>
+                <div><small>{mystreaks.current.start} - {mystreaks.current.end}</small></div>
+            </span>);
+        }
+        
+        return (<div>
+            <a href="{ profile.uri }">
+                <img src={ profile.photo_url } />;
+                <h1>{ profile.display_name }</h1>
+            </a>
+            <p><strong>{ profile.hashtags.join(' ') }</strong></p>
+            <div>
+                All Time: { profile.total_miles } Miles<br />
+                2014: { profile.yearly_miles } Miles
+            </div>
+            {streaks}
+        </div>);
+    }
+});
+
+//$(document).ready(function() {
+  var uri = window.location.pathname;
+  var user_uri = uri.split("/").slice(0, 3).join("/");
+
+  $.get(user_uri + "/data/profile.json", function(data) {
+    React.renderComponent(
+      <ProfileSidebar profile={data} />,
+      document.getElementById("profile-display"));
+  });
+//});
