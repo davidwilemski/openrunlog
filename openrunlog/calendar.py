d2a3149adc7124bb3105076202e30a19fc687bbf
David Wilemski <david@davidwilemski.com>
2014-03-27 16:16:54 -0400

    remove logging in CalendarMilesHandler and put db call in thread

--- openrunlog/calendar.py
+++ openrunlog/calendar.py
@@ -47,7 +47,10 @@ class CalendarMilesHandler(base.BaseHandler):
 
         #calendar_runs = yield self.execute_thread(
         #    models.Run.get_calendar_runs, profile)
-        calendar_runs = models.Run.get_calendar_runs(profile)
-        distances = [x.distance for day in calendar_runs for x in day['runs']]
-        logging.debug(distances)
+        def calculate_miles():
+            calendar_runs = models.Run.get_calendar_runs(profile)
+            distances = [x.distance for day in calendar_runs for x in day['runs']]
+            return distances
+
+        distances = yield self.thread_pool.submit(calculate_miles)
         self.finish(str(sum(distances)))
